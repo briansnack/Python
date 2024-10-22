eedafab47f02f9c87f288a0a7567ccaf3d5dd6c3
briansnack <snackbrian10@gmail.com>
2024-10-22 11:14:49 -0300

    Métodos para adicionar itens

--- oo-sabor-express/app.py
+++ oo-sabor-express/app.py
@@ -5,6 +5,8 @@ from modelos.cardapio.prato import Prato
 restaurante_praca = Restaurante("Praça", "Gourmet")
 bebida_suco = Bebida("Suco de Melancia", 5, "Grande")
 prato_paozinho = Prato("Paozinho", 2, "O melhor pão da cidade")
+restaurante_praca.adicionar_bebida_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_prato_no_cardapio(prato_paozinho)
 
 def main():
     print(bebida_suco)
