7b3815d2c512da84e85ad2404332f004e02166b3
briansnack <snackbrian10@gmail.com>
2024-10-22 11:54:18 -0300

    Polimorfismo

--- oo-sabor-express/app.py
+++ oo-sabor-express/app.py
@@ -3,8 +3,13 @@ from modelos.cardapio.bebida import Bebida
 from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante("Praça", "Gourmet")
+
 bebida_suco = Bebida("Suco de Melancia", 5, "Grande")
+bebida_suco.aplicar_desconto()
+
 prato_paozinho = Prato("Paozinho", 2, "O melhor pão da cidade")
+prato_paozinho.aplicar_desconto()
+
 restaurante_praca.adicionar_no_cardapio(bebida_suco)
 restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
