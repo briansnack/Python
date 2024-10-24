8da3004515c2075e98705634dc5e60b06c62f7b6
briansnack <snackbrian10@gmail.com>
2024-10-24 13:52:26 -0300

    Consultando um endpoint

--- oo-sabor-express-virtual/main.py
+++ oo-sabor-express-virtual/main.py
@@ -5,10 +5,16 @@ app = FastAPI()
 
 @app.get("/api/hello")
 def hello_world():
+    """
+    Endpoint que exibe uma mensagem incrível do mundo da programação
+    """
     return {"Hello": "World"}
 
-@app.get("/api/restaurantes")
+@app.get("/api/restaurantes/")
 def get_restaurantes(restaurante: str = Query(None)):
+    """
+    Endpoint para ver os cardárpios dos restaurantes
+    """
     url = "https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json"
     response = requests.get(url)
 
